6a45da4d750533d8d50c7cac77c28e2953a0a97f
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-02-24 00:26:39 -0800

    update code snippet

--- clicky-memory-game/src/components/Thumbnails.js
+++ clicky-memory-game/src/components/Thumbnails.js
@@ -3,14 +3,6 @@ import React from "react";
 
 function ImageThumb(props) {
     return (
-        // <div className="card">
-        //     <div className="img-container">
-        //         <img alt={props.name} src={props.image} />
-        //     </div>
-        // </div>
-
-        // TESTING STUFF 
-        // <div>
         <div className="col-3 shuffle">
             {/* Where does the onclick go? */}
             <img alt={props.name} src={props.image} className="img-thumbnail" onClick={() => props.handleShuffle(props.id, props.clicked)} />
